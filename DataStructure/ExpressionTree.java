7937df1385be7a9a71e7b52281d26b1acab00f42
Juan Lee <juanlee@kaist.ac.kr>
2021-12-26 17:00:09 +0900

    Add answers for ExpressionTree

--- DataStructure/ExpressionTree.java
+++ DataStructure/ExpressionTree.java
@@ -30,16 +30,15 @@ public class ExpressionTree {
     // 1. only two operand if no bracket. e.g. 1+2+3 will be given as (1+2)+3
     // 2. number is one-digit positive integer
     public static BinaryTree construct(String exp) {
-        System.out.println(exp);
         BinaryTree root = new BinaryTree();
 
         /* left operand */
         int opIndex = 1; // index of operator
-        if (Character.isDigit(/*** (1) ***/)) {
-            root.setLeft(/*** (2) ***/);
+        if (Character.isDigit(exp.charAt(0))) {
+            root.setLeft(exp.substring(0, 1));
         } else {
-            int index = closingIndex(exp, /*** (3) ***/);
-            BinaryTree left = construct(/*** (4) ***/);
+            int index = closingIndex(exp, 0);
+            BinaryTree left = construct(exp.substring(1, index));
             left.parent = root;
             root.left = left;
 
@@ -50,14 +49,27 @@ public class ExpressionTree {
         root.elem = Character.toString(exp.charAt(opIndex));
 
         /* right operand */
-        if (Character.isDigit(/*** (5) ***/)) {
+        if (Character.isDigit(exp.charAt(opIndex + 1))) {
             root.setRight(exp.substring(opIndex + 1, opIndex + 2));
         } else {
-            BinaryTree right = construct(exp.substring(/*** (6) ***/, exp.length() - 1));
+            BinaryTree right = construct(exp.substring(opIndex + 2, exp.length() - 1));
             right.parent = root;
             root.right = right;
         }
 
         return root;
+    }
+
+    // calculate the constructed tree
+    public static double calculate(BinaryTree tree) {
+        //TODO: for next round
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        BinaryTree tree = construct("((1+2)*(5-2))/2");
+        double result = calculate(tree);
+
+        System.out.println(result);
     }
 }
